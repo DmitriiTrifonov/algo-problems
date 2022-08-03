d6695b9c8dab87daad63a493e7b98a14b59229bb
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2022-08-03 18:45:16 +0300

    Update 1431-kids_with_the_greatest_number_of_candies.go

--- leet/easy/1431-kids_with_the_greatest_number_of_candies.go
+++ leet/easy/1431-kids_with_the_greatest_number_of_candies.go
@@ -1,18 +1,22 @@
 // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
-// Max sets every step
+// Two cycles: one for max, one for extra
 
 func kidsWithCandies(candies []int, extraCandies int) []bool {
     out := make([]bool, 0, len(candies))
     max := 0
     for _, c := range candies {
-        if c + extraCandies > max {
-            max = c + extraCandies
+        if c > max {
+            max = c
+        }
+    }
+    for _, c := range candies {
+        num := c + extraCandies
+        if num >= max {
             out = append(out, true)
             continue
         }
         out = append(out, false)
-        max = c + extraCandies
     }
     return out
 }
