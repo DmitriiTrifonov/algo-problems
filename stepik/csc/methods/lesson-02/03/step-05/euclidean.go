25e11e975e790d4d1fa3fe443eebd21fc0583309
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2022-08-18 12:21:59 +0300

    Update euclidean.go

--- stepik/csc/methods/lesson-02/03/step-05/euclidean.go
+++ stepik/csc/methods/lesson-02/03/step-05/euclidean.go
@@ -1,1 +1,25 @@
+// https://stepik.org/lesson/13229/step/5?unit=3415
 
+package main
+
+import "fmt"
+
+func main() {
+    var a, b int
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    fmt.Println(euclidean(a,b))
+}
+
+func euclidean(a, b int) int {
+    if a == 0 {
+        return b
+    }
+    if b == 0 {
+        return a
+    }
+    if a >= b {
+        return euclidean(a % b, b)
+    }
+    return euclidean(a, b % a)
+}
